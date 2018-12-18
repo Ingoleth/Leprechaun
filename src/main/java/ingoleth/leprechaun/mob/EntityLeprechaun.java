0586fd87addbec1775d06eca952ebd8d8237c204
Ingoleth <aiglesia97@gmail.com>
2018-12-18 15:50:24 +0100

    More woek on the leprechaun
    Leprechaun is now immune to fall damage;
    It will also float up and remain in the air;
    Placed a damage cap;

--- src/main/java/ingoleth/leprechaun/mob/EntityLeprechaun.java
+++ src/main/java/ingoleth/leprechaun/mob/EntityLeprechaun.java
@@ -3,6 +3,7 @@ package ingoleth.leprechaun.mob;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAILookIdle;
@@ -10,10 +11,17 @@ import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.EntityMoveHelper;
+
+import javax.annotation.Nonnull;
+
+import ingoleth.leprechaun.mob.EntityLeprechaun;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class EntityLeprechaun extends EntityCreature {
@@ -30,7 +38,7 @@ public class EntityLeprechaun extends EntityCreature {
 	        super.applyEntityAttributes();
 	        // Here we set various attributes for our mob. Like maximum health, armor, speed, ...
 	        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-	        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.13D);
+	        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.4D);
 	        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(2.0D);
 	        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(50.0D);
 	    }
@@ -40,7 +48,7 @@ public class EntityLeprechaun extends EntityCreature {
 	        this.tasks.addTask(0, new EntityAISwimming(this));
 	        this.tasks.addTask(2, new EntityAIAttackMelee(this, 1.0D, false));
 	        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
-	        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+	        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
 	        this.tasks.addTask(8, new EntityAILookIdle(this));
 	        this.targetTasks.addTask(3, new EntityAINearestAttackableTarget<>(this, EntityLiving.class, 4, false, true,
 	                entity -> entity != null && IMob.VISIBLE_MOB_SELECTOR.apply(entity)));
@@ -56,5 +64,32 @@ public class EntityLeprechaun extends EntityCreature {
 	        this.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_STRONG, 1.0F, 1.0F);
 	        return entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float)(7 + this.rand.nextInt(15)));
 	    }
+	    
+	    @Override
+		public void fall(float distance, float damageMultiplier) {
+		}
+	    
+	 
+	    
+	    public void onUpdate()
+	    {//Lift off the ground & stay there
+	        this.noClip = false;
+	        super.onUpdate();
+	        this.noClip = false;
+	        this.setNoGravity(true);
+	        if (this.onGround) {
+	        	this.motionY = 0.06;
+	        }
 
+	    }
+	    
+	    public boolean attackEntityFrom(@Nonnull DamageSource source, float par2) {
+			//Damage cap
+				int cap = 5;
+				return super.attackEntityFrom(source, Math.min(cap, par2));
+			}
+
+
+	    
+	    
 }
